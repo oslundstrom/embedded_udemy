2e779d2c1c25d108322f32f41ad361e89da46ce7
Oskar Lundström <oslundstrom@gmail.com>
2024-12-05 15:55:46 +0100

    Add type sizes to print output

--- hello_usb.c
+++ hello_usb.c
@@ -11,6 +11,13 @@ int main() {
     stdio_init_all();
     while (true) {
         printf("Hello, world!\n");
-        sleep_ms(1000);
+        printf("Size of char: %u", sizeof(char));
+        printf("Size of int: %u", sizeof(int));
+        printf("Size of short: %u", sizeof(short));
+        printf("Size of long: %u", sizeof(long));
+        printf("Size of long long: %u", sizeof(long long));
+        printf("Size of double: %u", sizeof(double));
+        printf("Size of float: %u", sizeof(float));
+        sleep_ms(5000);
     }
 }
